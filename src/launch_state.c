a08ffa5a759cb22ddb69d456d128a61220f5c352
rmntgx <rmntgx@gmail.com>
2025-05-21 13:33:15 +0500

    fix: simplified code

--- src/launch_state.c
+++ src/launch_state.c
@@ -7,21 +7,13 @@
 
 #include "launch_state.h"
 
-static void serialize_lstate(FILE* f, struct LaunchState st) {
-	fprintf(f, "%i;%i", st.user_opt, st.session_opt);
-}
-
-static void deserialize_lstate(FILE* f, struct LaunchState* st) {
-	fscanf(f, "%i;%i", &st->user_opt, &st->session_opt);
-}
-
 struct LaunchState read_launch_state() {
 	struct LaunchState state;
 	state.user_opt = 1;
 	state.session_opt = 1;
 	FILE* f = fopen(STATE_PATH, "r");
 	if(!f) return state;
-	deserialize_lstate(f, &state);
+	fscanf(f, "%i;%i", &state.user_opt, &state.session_opt);
 	fclose(f);
 	return state;
 }
@@ -29,7 +21,7 @@ struct LaunchState read_launch_state() {
 bool write_launch_state(struct LaunchState state) {
 	FILE* f = fopen(STATE_PATH, "w");
 	if(!f) return false;
-	serialize_lstate(f, state);
+	fprintf(f, "%i;%i", state.user_opt, state.session_opt);
 	fclose(f);
 	return true;
 }
