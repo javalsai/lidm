93f007691e4483ed781817e4db60ce25fccbe237
javalsai <jvssxxi@gmail.com>
2025-06-06 20:27:01 +0200

    fix(aur): package testing script

--- assets/pkg/aur/test-makepkg.sh
+++ assets/pkg/aur/test-makepkg.sh
@@ -5,8 +5,14 @@ MYSELF=$(realpath "$0")
 MYDIR=$(dirname "$MYSELF")
 
 for pkg in "$MYDIR"/*/; do
-    cd "$pkg"
     printf "\x1b[1mEntering '%s'\x1b[0m\n" "$pkg"
+    cd "$pkg"
+
+    # shellcheck disable=SC1091
+    source "PKGBUILD"
+    for source in "${source[@]}"; do
+        awk -F'::' '{print $1}' <<<"$source" | xargs rm -rf
+    done
 
     rm -rf ./*.{gz,zst} src pkg
     makepkg -f .
