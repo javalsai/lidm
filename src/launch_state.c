16d1325f96f7823289c1d1978e40bf679aaf376a
rmntgx <rmntgx@gmail.com>
2025-06-29 10:56:31 +0500

    style: formatting and linter satisfaction

--- src/launch_state.c
+++ src/launch_state.c
@@ -1,27 +1,27 @@
-// Small file to save last selection
+// Small file for saving last selection
 
 #define STATE_PATH "/var/lib/lidm/state"
-
-#include <stdio.h>
-#include <stdbool.h>
 
 #include "launch_state.h"
 
 struct LaunchState read_launch_state() {
-	struct LaunchState state;
-	state.user_opt = 1;
-	state.session_opt = 1;
-	FILE* f = fopen(STATE_PATH, "r");
-	if(!f) return state;
-	fscanf(f, "%i;%i", &state.user_opt, &state.session_opt);
-	fclose(f);
-	return state;
+  struct LaunchState state;
+  state.user_opt = 1;
+  state.session_opt = 1;
+  FILE* state_fd = fopen(STATE_PATH, "r");
+  if (!state_fd) return state;
+  if (fscanf(state_fd, "%i;%i", &state.user_opt, &state.session_opt) != 2) {
+    state.user_opt = 1;
+    state.session_opt = 1;
+  }
+  (void)fclose(state_fd);
+  return state;
 }
 
 bool write_launch_state(struct LaunchState state) {
-	FILE* f = fopen(STATE_PATH, "w");
-	if(!f) return false;
-	fprintf(f, "%i;%i", state.user_opt, state.session_opt);
-	fclose(f);
-	return true;
+  FILE* state_fd = fopen(STATE_PATH, "w");
+  if (!state_fd) return false;
+  (void)fprintf(state_fd, "%i;%i", state.user_opt, state.session_opt);
+  (void)fclose(state_fd);
+  return true;
 }
