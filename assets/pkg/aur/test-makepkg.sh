e529483c28dd4f137596cde417cab0a48945efd4
javalsai <jvssxxi@gmail.com>
2025-07-21 18:25:35 +0200

    fix(pkg,aur): sources now are version unique

--- assets/pkg/aur/test-makepkg.sh
+++ assets/pkg/aur/test-makepkg.sh
@@ -7,15 +7,6 @@ MYDIR=$(dirname "$MYSELF")
 for pkg in "$MYDIR"/*/; do
     printf "\x1b[1mEntering '%s'\x1b[0m\n" "$pkg"
     cd "$pkg"
-
-    # shellcheck disable=SC1091
-    source "PKGBUILD"
-    for source in "${source[@]}"; do
-        awk -F'::' '{print $1}' <<<"$source" | xargs rm -rf
-    done
-
-    rm -rf ./*.{gz,zst} src pkg
     makepkg -f .
-
     echo
 done
