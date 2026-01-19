133c5a378f4c7985262463637dd0f9fb5cf5b43c
javalsai <jvssxxi@gmail.com>
2026-01-19 20:45:51 +0100

    fix: signal_handler's header fixes
    - add include guard
    - include headers in source file

--- include/signal_handler.h
+++ include/signal_handler.h
@@ -1,3 +1,8 @@
+#ifndef SIGNALHANDLERH_
+#define SIGNALHANDLERH_
+
 // handle SIGTERM by sending SIGTERM to all children, resulting
 // in a graceful graphical shutdown
 void setup_sigterm();
+
+#endif /* SIGNALHANDLERH_ */
--- src/signal_handler.c
+++ src/signal_handler.c
@@ -3,6 +3,8 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+#include "signal_handler.h"
+
 static void handle_sigterm(int sig) {
   (void)sig;
 
